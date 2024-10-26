628eab00a69dd137ef2388a523dc501aaf286497
JP <679434+npgenx@users.noreply.github.com>
2024-10-26 15:30:14 -0700

    fix typo

--- constants/metadata.tsx
+++ constants/metadata.tsx
@@ -6,14 +6,6 @@ export const siteMeta = {
     contact: 'jp-morgan.dev@npgenx.com'
 };
 
-export const about = `
-          A Senior Frontend Developer with over 10 years of experience in front-end and full-stack web development. Adept at building robust, scalable web applications using React, JavaScript (ES6+), and modern libraries. Strong background in state management (Redux), component-driven architecture, and performance optimization. Experienced in leading teams and collaborating in Agile environments, with a proven track record in high-profile companies such as Disney, Fox Digital Media, and Sony Pictures.
-          <br />
-          <br />
-          With a fervor for crafting elegant solutions, I navigate the
-          ever-evolving landscape of software development. My journey involves
-          translating concepts into code, creating seamless user experiences,
-          and constantly pushing the boundaries of what&#39;s possible.
-          `;
+export const about = `A Senior Frontend Developer with over 10 years of experience in front-end and full-stack web development. Adept at building robust, scalable web applications using React, JavaScript (ES6+), and modern libraries. Strong background in state management (Redux), component-driven architecture, and performance optimization. Experienced in leading teams and collaborating in Agile environments, with a proven track record in high-profile companies such as Disney, Fox Digital Media, and Sony Pictures. With a fervor for crafting elegant solutions, I navigate the ever-evolving landscape of software development. My journey involves translating concepts into code, creating seamless user experiences,  and constantly pushing the boundaries of what's possible.`;
 
 export const bannerData = `Passionate Software Engineer with a focus on frontend development. Can usually can be found just coding or at least thinking about it while sitting on this rock speeding 107,218 km/h`
